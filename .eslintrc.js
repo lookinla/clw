e1db42fd586d812cb0f545c8d057def7d5000829
Ashkan Kardan <ashkankardan14@gmail.com>
2021-07-16 23:59:33 +0300

    initial next app setup

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['get-off-my-lawn'],
+  rules: {
+      'no-param-reassign': 0,
+      'node/no-unpublished-require': 0,
+      'react/display-name': 0,
+      'react/jsx-no-literals': 0,
+      'jsx-ally/author-is-valid': 0
+      'react/react-in-jsx-scope': 0,
+  }
+};
